8e04c87338da159e5615b93d07d0eb645fd76521
Jay Kan <leicasper@gmail.com>
2016-08-04 12:30:55 -0700

    feat(autofocus): add autofocus directive

---
+++ src/components/modal/autofocus.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, ElementRef } from '@angular/core';
+import { ModalComponent } from './modal';
+
+@Directive({ selector: '[autofocus]' })
+export class AutofocusDirective {
+  constructor(private _el: ElementRef,
+              private _modal: ModalComponent) {
+    if (_modal !== null) {
+      this._modal.onOpen.subscribe(() => {
+        this._el.nativeElement.focus();
+      });
+    }
+  }
+}
