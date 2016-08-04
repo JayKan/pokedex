d1b3d265ce4f696ab5a3841292b8b873188c2b7e
Jay Kan <leicasper@gmail.com>
2016-08-04 12:31:40 -0700

    feat(modal): add common modal utilities

---
+++ src/components/modal/modal.utils.ts
@@ -0,0 +1,25 @@
+import { Observable } from 'rxjs/Observable';
+
+function booleanOrValue(value) {
+  if (value === 'true')
+    return true;
+  else if (value === 'false')
+    return false;
+  return value;
+}
+
+function toPromise<T>(observable: Observable<T>): Promise<T> {
+  return new Promise((resolve, reject) => {
+    observable.subscribe(next => {
+      resolve(next);
+    }, error => {
+      reject(error);
+    });
+  });
+}
+
+export {
+  booleanOrValue,
+  toPromise
+};
+
