eb4a6696c4f67b94d0ca3d9398b8c6d3feccf300
Jay Kan <leicasper@gmail.com>
2016-08-07 18:27:47 -0700

    chore: add project vendor files

---
+++ src/vendor.ts
@@ -0,0 +1,16 @@
+// Angular2 related
+import '@angular/common';
+import '@angular/core';
+import '@angular/forms';
+import '@angular/platform-browser-dynamic';
+import '@angular/router';
+import '@angular/http';
+
+// RxJS monkey-patch related operators
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/fromEvent';
+
+
+
